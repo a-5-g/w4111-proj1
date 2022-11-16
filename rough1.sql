f3012f82524dabca219fc651b585bf7431588d83
amol <amolgupta99@gmail.com>
2022-11-16 14:41:57 -0500

    Submission

--- rough1.sql
+++
@@ -1,4 +0,0 @@
-
-CREATE SEQUENCE orderidseq OWNED BY placesorder.orderid;
-SELECT SETVAL('orderidseq', (select max(orderid)+1 from placesorder), false);
-ALTER TABLE placesorder ALTER COLUMN orderid SET DEFAULT nextval('orderidseq');
